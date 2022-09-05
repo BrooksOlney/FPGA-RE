3c47a272c9f7c8c27a739d1d74c5e41f50652867
BrooksOlney <brooks.olney@gmail.com>
2022-09-05 01:01:32 -0400

    update

--- bitstream.py
+++ bitstream.py
@@ -15,19 +15,37 @@ class Bitstream:
     def parse_bits(self):
         
         # word occurs after the main file header
-        sync = 0xFFFFFFFFAA995566
-        syncWord = sync.to_bytes(8, 'big')
+        sync = 0xFFFFFFFFFFFFFFFFAA995566
+        syncWord = sync.to_bytes(12, 'big')
         rawBytes = self._raw_bytes.tobytes()
-
-        self.header = self._raw_bytes[:rawBytes.find(syncWord)]
-        rem = self._raw_bytes[rawBytes.find(syncWord) + 8:]
-        configPackets = rem.reshape(-1,4).view(np.uint32).flatten()
-        configBits = np.unpackbits(configPackets, 1)
-        packetTypes = configPackets >> 29
+        syncIdx = rawBytes.find(syncWord)
         
+        busDesc = rawBytes[syncIdx-8:syncIdx]
+        busWidthSync = int.from_bytes(busDesc[:4],'big')
+        busWidthDetect = int.from_bytes(busDesc[4:], 'big')
+        
+        self.header = self._raw_bytes[:rawBytes.find(syncWord)]
+        rem = self._raw_bytes[rawBytes.find(syncWord) + 12:]
+        configPackets = rem.reshape(-1,4).view(np.uint32).flatten()
+        test = [hex(pkt)[2:].zfill(8) for pkt in configPackets]
+        # configBits = np.unpackbits(configPackets, 1)
+        packetTypes = configPackets >> 29
+        pktsTyped = []
+        pktsTypedInds = []
+        for i in range(8):
+            pktsTypedInds.append(np.where(packetTypes == i)[0].tolist())
+            pktsTyped.append(configPackets[pktsTypedInds[-1]].tolist())
+
+
+
         p1NOP = 0b00
         p1Read = 0b01
         p1Write = 0b10
+
+        nopPackets = np.where(packetTypes == p1NOP)
+        readPackets = np.where(packetTypes == p1Read)
+        writePackets = np.where(packetTypes == p1Write)
+
 
         print('hi')
 
